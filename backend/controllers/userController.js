c3b57116b83c123de1dc5706186b087cb57c59a0
Michael-Njoroge <mikethecoder12@gmail.com>
2023-11-01 10:19:17 +0300

    create user controller, user route and user model

---
+++ backend/controllers/userController.js
@@ -0,0 +1,24 @@
+//@desc    Register new user
+//@route    POST /api/users
+//@access   Public
+const registerUser = (req, res) => {
+    res.json({message: 'Register User'})
+}
+
+//@desc    Authenticate new user
+//@route    POST /api/users/login
+//@access   Public
+const loginUser = (req, res) => {
+    res.json({message: 'Login User'})
+}
+
+//@desc    Get user data
+//@route    GET /api/users/me
+//@access   Private
+const getMe = (req, res) => {
+    res.json({message: 'Display User data'})
+}
+
+module.exports = {
+    registerUser,loginUser,getMe
+}
